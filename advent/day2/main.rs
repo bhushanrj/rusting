0d375223ab374178167149e6b78c3e63d148a2e1
Bhushan Jahagirdar <j.bhushan.r@gmail.com>
2024-12-11 21:31:21 +0000

    Add day2 logic

--- advent/day2/main.rs
+++ advent/day2/main.rs
@@ -7,19 +7,53 @@ fn read_lines(file_path: &str) -> Result<Lines<BufReader<File>>> {
     Ok(BufReader::new(file).lines())
 }
 
+
+fn analyze_report(data: &Vec<i32>) -> bool {
+    if data.windows(2).all(|x|x[0] < x[1]) {
+        if data.windows(2).all(|x|x[1] - x[0] <=0 ) {
+            false
+        } else if data.windows(2).all(|x|x[1] - x[0] > 3) {
+            false
+        } else {
+            true
+        }
+    } else if data.windows(2).all(|x|x[0] > x[1]) {
+        if data.windows(2).all(|x|x[0] - x[1] <=0 ) {
+            false
+        } else if data.windows(2).all(|x|x[0] - x[1] > 3) {
+            false
+        } else {
+            true
+        }
+    } else {
+        false
+    }
+}
+
 fn main() {
-    let mut data: Vec<i32> = Vec::new();
+    let mut report: u32 = 0;
     match read_lines("./data.txt") {
         Ok(lines) => {
             for line in lines {
                 if let Ok(d) = line {
-                    let dt: Vec<&str>= d.split(" ").collect();
-                    data = dt.iter().parse::<i32>().unwrap().collect();
-                    println!("print data {:?} \n", data[0]);
+                    let dt: Vec<_> = d.split(" ").collect();
+                    println!("print dt {:?} \n", dt);
+                    let data_res: Result<Vec<i32>, _>  = dt.iter().map(|x| x.parse::<i32>()).collect();
+                    match data_res {
+                        Ok(data) => {
+                            let res = analyze_report(&data);
+                            println!("print res {}", res);
+                            if res  {
+                                report = report + 1;
+                            }
+                        }
+                        Err(e) => eprintln!("Failed to convert: {}", e)
+                    }
                 }
             }
         }
-        Err(e) => println!("Error reading file: {}", e),
+        Err(e) => eprintln!("Error reading file: {}", e),
     }
-    //println!("print data {:?}", data);
+
+    println!("Report {}", report);
 }
